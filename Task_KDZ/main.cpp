f1bc8ce2781e69596c57d6129de4077e2994773e
Matvey Kravtsov <kravtsovguy@gmail.com>
2016-11-28 23:06:12 +0300

    added info about the project in main.cpp

--- Task_KDZ/main.cpp
+++ Task_KDZ/main.cpp
@@ -6,6 +6,18 @@
 //  Copyright © 2016 Matvey Kravtsov. All rights reserved.
 //
 
+// КДЗ-1 по дисциплине Алгоритмы и структуры данных
+// Кравцов Матвей Евгеньевич, БПИ152, 29.11.2016
+// Xcode 8.1, компилятор Clang, состав проекта:
+//------------------------------------
+// huffman.cpp      | huffman.h
+// shannon_fano.cpp | shannon_fano.h
+// timer.cpp        | timer.h
+// random_text.cpp  | random_text.h
+// tester.cpp       | tester.h
+//------------------------------------
+// Сделано всё, что написано в файле задания
+
 #include <iostream>
 #include "huffman.h"
 #include "shannon_fano.h"
@@ -19,6 +31,6 @@ using namespace std;
 int main(int argc, const char * argv[])
 {
     Tester().test();
-    
+
     return 0;
 }
